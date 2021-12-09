718a198aea08044b6b7e18c3eef7a0ddc370f061
Oeecheverria <93943356+Oeecheverria@users.noreply.github.com>
2021-12-09 11:45:39 -0300

    Delete Android/EmpleadosCapas directory

--- Android/EmpleadosCapas/app/src/main/java/com/example/empleadoscapas/Presentacion/Constantes.java
+++
@@ -1,7 +0,0 @@
-package com.example.empleadoscapas.Presentacion;
-
-public class Constantes {
-
-    public static final String MIS_LOGS = "MIS_LOGS";
-    public static final String EXTRA_EMPLEADO = "EXTRA_EMPLEADO";
-}
