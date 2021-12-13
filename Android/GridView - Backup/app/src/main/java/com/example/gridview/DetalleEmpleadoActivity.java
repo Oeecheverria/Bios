95a9bde6b36ae2f27e5715b8ea139d5248eb86c3
Oscar Echeverria <oecheverria22@gmail.com>
2021-12-13 09:55:36 -0300

    f

---
+++ Android/GridView - Backup/app/src/main/java/com/example/gridview/DetalleEmpleadoActivity.java
@@ -0,0 +1,41 @@
+package com.example.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetalleEmpleadoActivity extends AppCompatActivity {
+
+    protected ImageView imgvAvatar;
+    protected TextView tvCedula;
+    protected TextView tvNombreCompleto;
+    protected TextView tvDescripcion;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detalle_empleado);
+
+        imgvAvatar = (ImageView)findViewById(R.id.imgvAvatar);
+        tvCedula = (TextView)findViewById(R.id.tvCedula);
+        tvNombreCompleto = (TextView)findViewById(R.id.tvNombreCompleto);
+        tvDescripcion = (TextView)findViewById(R.id.tvDescripcion);
+
+        //Empleado empleado = new Empleado(getIntent().getIntExtra(MainActivity.EXTRA_CEDULA, 0), getIntent().getStringExtra(MainActivity.EXTRA_NOMBRE_COMPLETO), getIntent().getStringExtra(MainActivity.EXTRA_DESCRIPCION));
+
+        Empleado empleado = (Empleado)getIntent().getSerializableExtra(MainActivity.EXTRA_EMPLEADO);
+
+        int idImagenAvatar = getResources().getIdentifier("empleado_" + empleado.getCedula(),
+                "drawable", getPackageName());
+
+        imgvAvatar.setImageResource(idImagenAvatar);
+        tvCedula.setText(String.valueOf(empleado.getCedula()));
+        tvNombreCompleto.setText(empleado.getNombreCompleto());
+        tvDescripcion.setText(empleado.getDescripcion());
+    }
+
+}
