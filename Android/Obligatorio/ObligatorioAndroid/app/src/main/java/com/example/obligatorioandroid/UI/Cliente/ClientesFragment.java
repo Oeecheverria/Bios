579bbc34afff8050db004050b748f3135084a9fa
Oeecheverria <oscar.echeverria@sabre.com>
2021-12-27 14:38:00 -0300

    All done

---
+++ Android/Obligatorio/ObligatorioAndroid/app/src/main/java/com/example/obligatorioandroid/UI/Cliente/ClientesFragment.java
@@ -0,0 +1,11 @@
+package com.example.obligatorioandroid.UI.Cliente;
+
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+public class ClientesFragment extends Fragment {
+
+    protected ListView lvClientes;
+
+}
---
+++ Android/Obligatorio/ObligatorioAndroid/app/src/main/java/com/example/obligatorioandroid/UI/Cliente/DetalleClienteFragment.java
@@ -0,0 +1,7 @@
+package com.example.obligatorioandroid.UI.Cliente;
+
+import androidx.fragment.app.Fragment;
+
+public class DetalleClienteFragment extends Fragment {
+    
+}
